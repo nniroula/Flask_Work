7a1b0a8acbdaf2d2111f62d3345e98e906a95be9
Nabin <nkbhaie2017@gmail.com>
2021-09-01 14:53:43 -0600

    add a file that has name different than app.py name

---
+++ my_app.py
@@ -0,0 +1,8 @@
+
+""" This file is to test that flask does not run non app.py files by default. You have to set environmental variable to 
+run it. set as:
+FLASK_APP=my_file_name.py flask run
+"""
+from flask import Flask
+
+app = Flask(__name__)
