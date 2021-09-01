7dccee4d8481b13e6ae1086b1d3a8df49bb57439
Nabin <nkbhaie2017@gmail.com>
2021-09-01 15:20:13 -0600

    add html file content to the view function and return that html content

--- app.py
+++ app.py
@@ -3,7 +3,37 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+# @app.route("/")
+# def hello():
+#     return "HELLO"
+
+@app.route("/goodbye")
+def goodbye():
+    return "Good Bye"
+
+# now return html content
+@app.route("/hello")
 def hello():
-    return "HELLO"
+    html = """
+    <html>
+        <body>
+            <h1> Hello, Flask First App. </h1>
+            <p>This is an html content displayed here.</p>
+        </body>
+    </html>
+    """
+    return html
+
+@app.route("/")
+def home_page():
+    html = """
+    <html>
+        <body>
+            <h1> Hello, this is Home Page. </h1>
+            <p>Welcome to this simple app.</p>
+            <a href='/hello'>Go to Hello page</a>
+        </body>
+    </html>
+    """
+    return html
 
